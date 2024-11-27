7565d5ffdbed064b398cc362928ffed8b5e0dda4
hoeleong20 <chewhl2002@gmail.com>
2024-11-27 20:46:18 +0800

    Created User Account Entity

---
+++ TutionCetre/src/UserAccountModule/UserAccount.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserAccountModule;
+
+/**
+ *
+ * @author Hafiz Chew Hoe Leong
+ */
+public class UserAccount {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String userType;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+}
