1cb555182318f418c6315bf875dcb630a877b3a9
jielun <chiewjl-wm21@student.tarc.edu.my>
2024-11-30 16:08:23 +0800

    [finishes #188630182]

---
+++ TutionCetre/src/Student/Student.java
@@ -0,0 +1,48 @@
+package Student;
+import java.util.*;
+import CourseManagement.Course;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private List<Course> enrolledCourses;
+    
+    public Student(String studentId, String studentName, List<Course> enrolledCourses){
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public List<Course> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public void setEnrolledCourses(List<Course> enrolledCourses) {
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    public void displayCourses(){
+        System.out.println("Courses enrolled by " + studentName + " (ID: " + studentId + "):");
+        for(Course course: enrolledCourses) {
+            System.out.println(course);
+        }
+    }
+    
+    
+}
